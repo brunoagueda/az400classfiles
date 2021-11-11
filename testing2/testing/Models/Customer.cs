ea84bf90e701e44fc2037272a9ce97d21ed1ab8f
Bruno Agueda <brunoagueda@gmail.com>
2021-11-10 21:48:58 -0300

    first

---
+++ testing2/testing/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testing.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int pId, string pName)
+        {
+            Id = pId;
+            Name = pName;
+        }
+    }
+
+    
+}
